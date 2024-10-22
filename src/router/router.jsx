096de3098b4e4a9ca797d9009db4d51b5b4589b6
Patrik Eriksson <puttrik96@hotmail.com>
2024-10-22 14:13:05 +0200

    Added routes

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,10 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
 import SetTimer from "../pages/SetTimer";
+import Alarm from "../pages/Alarm";
+import DigitalTimer from "../pages/DigitalTimer";
+import Loading from "../pages/Loading";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element: <Loading />,
+  },
+  {
+    path: "/set-timer",
     element: <SetTimer />,
+  },
+  {
+    path: "/alarm",
+    element: <Alarm />,
+  },
+  {
+    path: "/digital-timer",
+    element: <DigitalTimer />,
   },
 ]);
 
